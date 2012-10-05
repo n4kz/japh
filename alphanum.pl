76133d4e0181231fe6884d5ded7957793546f2df
n4kz <nfokz@yandex.ru>
2012-10-05 23:10:19 +0400

    Alphanumeric JAPH

---
+++ alphanum.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+eval q q y y shift unpack y 12345 67890 y and print chr q
+foreach
+eval q y s qq nf ssn sst ssu ih an ssc sss ssu scf scs ssf ih pc scs ssf scp ih nh an aa scn scs ssf sc q and split y
